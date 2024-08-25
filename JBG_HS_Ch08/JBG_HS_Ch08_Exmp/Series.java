1893e573a9255564433ad2e32d10e66cbfcbceb7
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-08-25 12:17:14 +0530

    JBG_HS_Ch08_Exmp: Interface default method practical example (#9)

--- JBG_HS_Ch08/JBG_HS_Ch08_Exmp/Series.java
+++ JBG_HS_Ch08/JBG_HS_Ch08_Exmp/Series.java
@@ -8,5 +8,19 @@ public interface Series {
     int getNext();  // Reeturn next number in series.
     void reset();  // restart
     void setStart(int startValue);  // Set starting value
+
+    /**
+     * Purpose: Return an array that contains the next n elements
+     * in the series beyond the current element.
+     */
+    default int[] getNextArray(int n) {
+        int[] vals = new int[n];
+
+        for(var i = 0; i < n; i++) {
+            vals[i] = getNext();
+        }  // for loop: 
+
+        return vals;
+    }  // method getNextArray
 }  // interface Series
 
