0b1a066b7fabd155ef3a53ddc32e1ab5c3e80d63
Mallikarjuna00G <mallikarjuna00g@gmail.com>
2024-08-25 12:44:36 +0530

    JBG_HS_Ch08_Exmp: private methods in an interface (#9)

--- JBG_HS_Ch08/JBG_HS_Ch08_Exmp/Series.java
+++ JBG_HS_Ch08/JBG_HS_Ch08_Exmp/Series.java
@@ -14,6 +14,25 @@ public interface Series {
      * in the series beyond the current element.
      */
     default int[] getNextArray(int n) {
+        return getArray(n);
+    }  // method getNextArray
+
+    /**
+     * Purpose: Return an array that contains the next n elements
+     * in the series, after skipping elements.
+     */
+    default int[] skipAndGetNextArray(int skip, int n) {
+        // Skip the specified number of elements.
+        getArray(skip);
+
+        return getArray(n);
+    }  // method skipAndGetNextArray
+
+    /**
+     * Purpose: A private method that returns an array containing
+     * the next n elements.
+     */
+    private int[] getArray(int n) {
         int[] vals = new int[n];
 
         for(var i = 0; i < n; i++) {
@@ -21,6 +40,6 @@ public interface Series {
         }  // for loop: 
 
         return vals;
-    }  // method getNextArray
+    }  // method getArray
 }  // interface Series
 
